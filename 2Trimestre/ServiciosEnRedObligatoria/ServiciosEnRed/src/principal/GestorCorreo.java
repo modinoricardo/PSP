9f54e3adb7ef1e32118e1cf48e3bd97aa917da00
modinoricardo <modinoricardo@gmail.com>
2024-12-03 13:54:41 +0100

    Final dia 3/12/24

--- 2Trimestre/ServiciosEnRedObligatoria/ServiciosEnRed/src/principal/GestorCorreo.java
+++ 2Trimestre/ServiciosEnRedObligatoria/ServiciosEnRed/src/principal/GestorCorreo.java
@@ -24,38 +24,22 @@ public class GestorCorreo {
 	private MiAutorizacion miAutorizacion;
 
 	private void setPropiedadesServidorSMTP(String usuario, String password) {
-		misPropiedades = new Properties();
-		misPropiedades.put("mail.smtp.auth", "true");
-		misPropiedades.put("mail.smtp.starttls.enable", "true");
-		misPropiedades.put("mail.smtp.host", "smtp.gmail.com");
-		misPropiedades.put("mail.smtp.port", "587");
 
-		miAutorizacion = new MiAutorizacion(usuario, password);
-		miSesion = Session.getInstance(misPropiedades, miAutorizacion);
 	}
 
 	private Transport conectarServidorSMTP() throws MessagingException {
-		Transport transport = miSesion.getTransport("smtp");
-		transport.connect();
-		return transport;
+
 	}
 
 	private Message crearNucleoMensaje(String emisor, String destinatario, String asunto) throws AddressException, MessagingException {
-		Message mensaje = new MimeMessage(miSesion);
-		mensaje.setFrom(new InternetAddress(emisor));
-		mensaje.setRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));
-		mensaje.setSubject(asunto);
-		return mensaje;
+
 	}
 
 	private Message crearMensajeTexto(String emisor, String destinatario, String asunto, String texto) throws AddressException, MessagingException {
-	    Message mensaje = crearNucleoMensaje(emisor, destinatario, asunto);
-	    mensaje.setText(texto);
-	    return mensaje;
+
 	}
 
 	private Message crearMensajeTextoConAdjunto(String emisor, String destinatario, String asunto, File archivo) {
-		return null;
 
 	}
 
